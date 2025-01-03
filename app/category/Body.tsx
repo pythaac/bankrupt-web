9ac6083ae607f7b916279581b89b180236d0bc89
pythaac <pythaac@gmail.com>
2025-01-04 03:45:34 +0900

    ScrolledHalfBoard에 column, item을 인자로 받도록 수정 / custom scroll 해제 (주석처리)

--- app/category/Body.tsx
+++ app/category/Body.tsx
@@ -1,43 +1,49 @@
+"use client";
+
 import { 
     Flex,
-    HStack,
+    Text,
 } from "@chakra-ui/react";
 
-import ScrolledHalfBoard from '../component/ScrolledHalfBoard'
+import ScrolledHalfBoard from '../common/ScrolledHalfBoard'
 import styles from './category.module.css'
 
 export default function Body() {
-    const items = [
-        {index: 1, court: "인천지방법원"},
-        {index: 2, court: "서울회생법원"},
-        {index: 3, court: "인천지방법원"},
-        {index: 4, court: "서울회생법원"},
-        {index: 5, court: "인천지방법원"},
-        {index: 6, court: "서울회생법원"},
-        {index: 7, court: "인천지방법원"},
-        {index: 8, court: "서울회생법원"},
-        {index: 9, court: "인천지방법원"},
-        {index: 10, court: "서울회생법원"},
-        {index: 11, court: "인천지방법원"},
-        {index: 12, court: "서울회생법원"},
-        {index: 13, court: "인천지방법원"},
-        {index: 14, court: "서울회생법원"},
-        {index: 15, court: "인천지방법원"},
-        {index: 16, court: "서울회생법원"},
-        {index: 17, court: "인천지방법원"},
-        {index: 18, court: "서울회생법원"},
-        {index: 19, court: "인천지방법원"},
-        {index: 20, court: "서울회생법원"},
-        {index: 21, court: "인천지방법원"},
-        {index: 22, court: "서울회생법원"},
-        {index: 23, court: "인천지방법원"},
-        {index: 24, court: "서울회생법원"}
+    const categoryColumns = [
+        <Text>Category</Text>,
+        <Text>Icon</Text>
+    ]
+    const categoryItems = [
+        [<Text>채권</Text>, <Text>Icon</Text>],
+        [<Text>부동산</Text>, <Text>Icon</Text>],
+        [<Text>채권</Text>, <Text>Icon</Text>],
+        [<Text>부동산</Text>, <Text>Icon</Text>],
+        [<Text>채권</Text>, <Text>Icon</Text>],
+        [<Text>부동산</Text>, <Text>Icon</Text>],
+        [<Text>채권</Text>, <Text>Icon</Text>],
+        [<Text>부동산</Text>, <Text>Icon</Text>],
+        [<Text>채권</Text>, <Text>Icon</Text>],
+        [<Text>부동산</Text>, <Text>Icon</Text>],
+        [<Text>채권</Text>, <Text>Icon</Text>],
+        [<Text>부동산</Text>, <Text>Icon</Text>],
+    ]
+    const kewordsColumns = [
+        <Text>Keywords</Text>,
+        <Text>Icon</Text>
+    ]
+    const kewordsItems = [
+        [<Text>채무자</Text>, <Text>Icon</Text>],
+        [<Text>채무</Text>, <Text>Icon</Text>],
+        [<Text>채무자</Text>, <Text>Icon</Text>],
+        [<Text>채무</Text>, <Text>Icon</Text>],
+        [<Text>채무자</Text>, <Text>Icon</Text>],
+        [<Text>채무</Text>, <Text>Icon</Text>],
     ]
 
     return (
         <Flex className={styles.body_flex}>
-            <ScrolledHalfBoard items={items} />
-            <ScrolledHalfBoard items={items} />
+            <ScrolledHalfBoard columns={categoryColumns} items={categoryItems} />
+            <ScrolledHalfBoard columns={kewordsColumns} items={kewordsItems} />
         </Flex>
     )
 }
