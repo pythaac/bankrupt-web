b642387537086efb03a45761bce18c7c14672518
pythaac <pythaac@gmail.com>
2025-01-04 01:53:09 +0900

    CustomScrollbars 컴포넌트를 사용한 ScrolledHalfBoard 컴포넌트 추가가

---
+++ app/component/ScrolledHalfBoard.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { 
+    Box, 
+    Table,
+} from "@chakra-ui/react";
+
+
+import styles from './component.module.css'
+import CustomScrollbars from "./CustomScrollbars";
+
+interface Item {
+    index: number;
+    court: string;
+}
+
+function getTableHeader() {
+    return (
+        <Table.Header className={styles.table_header}>
+            <Table.Row>
+                <Table.ColumnHeader>번호</Table.ColumnHeader>
+                <Table.ColumnHeader>법원</Table.ColumnHeader>
+            </Table.Row>
+        </Table.Header>
+    )
+}
+
+function getTableBody(items: Array<Item>) {
+    return (
+        <Table.Body>
+            {items.map((item) => (
+                <Table.Row key={item.index} className={styles.table_row}>
+                    <Table.Cell>{item.index}</Table.Cell>
+                    <Table.Cell>{item.court}</Table.Cell>
+                </Table.Row>
+            ))}
+        </Table.Body>
+    )
+}
+
+export default function ScrolledHalfBoard({ items }: { items: Array<Item> }) {
+
+    return (
+        <Box>
+            <Box height="320px">
+                <Box width="40%" height="100%" maxH="320px">
+                    <CustomScrollbars>
+                        <Table.Root unstyled className={styles.table}>
+                            {getTableHeader()}
+                            {getTableBody(items)}
+                        </Table.Root>
+                    </CustomScrollbars>
+                </Box>
+            </Box>
+        </Box>
+    )
+}
