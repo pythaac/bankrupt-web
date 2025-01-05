342af055f0a4b995aa68ed4fc323e3cda72780a2
pythaac <pythaac@gmail.com>
2025-01-06 01:18:46 +0900

    Category/Kewords Icon을 IconButton으로 구현현

--- app/category/Body.tsx
+++ app/category/Body.tsx
@@ -2,43 +2,58 @@
 
 import { 
     Flex,
-    Text,
+    Text
 } from "@chakra-ui/react";
+import { IconButton } from "@chakra-ui/react"
+import { PiPlusBold } from "react-icons/pi";
+import { FaTrashCan } from "react-icons/fa6";
+import { ReactNode } from "react";
 
 import ScrolledHalfBoard from '../common/ScrolledHalfBoard'
 import styles from './category.module.css'
 
+function addTrashbin(items: Array<Array<ReactNode>>) {
+    items.map((row) => {
+        row.push(
+            <IconButton size="sm" variant="ghost">
+                <FaTrashCan color="black" />
+            </IconButton>
+        )
+    })
+}
+
 export default function Body() {
     const categoryColumns = [
         <Text>Category</Text>,
-        <Text>Icon</Text>
+        <IconButton size="sm" variant="outline">
+            <PiPlusBold color="black" />
+        </IconButton>
     ]
     const categoryItems = [
-        [<Text>채권</Text>, <Text>Icon</Text>],
-        [<Text>부동산</Text>, <Text>Icon</Text>],
-        [<Text>채권</Text>, <Text>Icon</Text>],
-        [<Text>부동산</Text>, <Text>Icon</Text>],
-        [<Text>채권</Text>, <Text>Icon</Text>],
-        [<Text>부동산</Text>, <Text>Icon</Text>],
-        [<Text>채권</Text>, <Text>Icon</Text>],
-        [<Text>부동산</Text>, <Text>Icon</Text>],
-        [<Text>채권</Text>, <Text>Icon</Text>],
-        [<Text>부동산</Text>, <Text>Icon</Text>],
-        [<Text>채권</Text>, <Text>Icon</Text>],
-        [<Text>부동산</Text>, <Text>Icon</Text>],
+        [<Text>채권</Text>], [<Text>부동산</Text>],
+        [<Text>채권</Text>], [<Text>부동산</Text>],
+        [<Text>채권</Text>], [<Text>부동산</Text>],
+        [<Text>채권</Text>], [<Text>부동산</Text>],
+        [<Text>채권</Text>], [<Text>부동산</Text>],
+        [<Text>채권</Text>], [<Text>부동산</Text>],
     ]
     const kewordsColumns = [
         <Text>Keywords</Text>,
-        <Text>Icon</Text>
+        <IconButton size="sm" variant="outline">
+            <PiPlusBold color="black" />
+        </IconButton>
     ]
     const kewordsItems = [
-        [<Text>채무자</Text>, <Text>Icon</Text>],
-        [<Text>채무</Text>, <Text>Icon</Text>],
-        [<Text>채무자</Text>, <Text>Icon</Text>],
-        [<Text>채무</Text>, <Text>Icon</Text>],
-        [<Text>채무자</Text>, <Text>Icon</Text>],
-        [<Text>채무</Text>, <Text>Icon</Text>],
+        [<Text>채무자</Text>],
+        [<Text>채무</Text>],
+        [<Text>채무자</Text>],
+        [<Text>채무</Text>],
+        [<Text>채무자</Text>],
+        [<Text>채무</Text>],
     ]
+
+    addTrashbin(categoryItems);
+    addTrashbin(kewordsItems);
 
     return (
         <Flex className={styles.body_flex}>
