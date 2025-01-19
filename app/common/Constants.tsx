381106d2113b3f481272a28d984b8f645d8d96d1
pythaac <pythaac@gmail.com>
2025-01-19 15:57:45 +0900

    Board, Category interface를 상수로 선언언

--- app/common/Constants.tsx
+++ app/common/Constants.tsx
@@ -1,2 +1,19 @@
 
 export const apiServerUrl = "http://localhost:8080";
+
+export interface Category {
+    id: number,
+    categoryName: string
+}
+
+export interface Board {
+    id: number,
+    court: string,
+    seller: string,
+    title: string,
+    uploaded: string,
+    due: string,
+    fileLink: string,
+    telephoneNumber: string,
+    categories: Array<Category>
+}
