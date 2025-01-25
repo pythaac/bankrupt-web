f22b5999d07356b0c2ebdb6896da1d7e9896a8eb
pythaac <pythaac@gmail.com>
2025-01-26 03:13:37 +0900

    ToastContext를 Layout에 적용

--- app/layout/Layout.tsx
+++ app/layout/Layout.tsx
@@ -1,28 +1,37 @@
-import { Box, Flex, HStack, VStack } from "@chakra-ui/react";
+"use client"
+
+
+import { Box, createToaster, CreateToasterReturn, Flex, HStack, VStack } from "@chakra-ui/react";
 import Header from "./Header";
 import Navigator from "./Navigator";
 
 import styles from './layout.module.css'
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
+import ToastComponent from "../common/ToastComponent";
+import { ToastContext } from "../common/Context";
 
 export default function Layout({children}: {children: ReactNode}) {
-    return (
-        <>
-            <Box className={styles.layout}>
-                <Header/>
-                <HStack gap="0" direction="row" height="100%">
-                    <Navigator/>
-                    <Box className={styles.body}>
-                        <Box 
-                            padding="50px"
-                            minW="1000px"
-                        >
-                            {children}
-                        </Box>
+    const toaster = createToaster({
+        placement: 'bottom',
+        overlap: true,
+      });
 
-                    </Box>
-                </HStack>
+    return (
+        <ToastContext.Provider value={toaster}>
+            <Box className={styles.layout}>
+                <ToastComponent>
+                    <Header/>
+                    <HStack gap="0" direction="row" height="100%">
+                        <Navigator/>
+                        <Box className={styles.body}>
+                            <Box padding="50px" minW="1000px">
+                                {children}
+                            </Box>
+
+                        </Box>
+                    </HStack>
+                </ToastComponent>
             </Box>
-        </>
+        </ToastContext.Provider>
     )
 }
